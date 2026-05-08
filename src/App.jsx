2b4f18625dd41d1b194eabd50f68e30f71427087
Shaahid <mohammadshaahid05@gmail.com>
2026-05-08 18:29:03 +0530

    Move

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Carousel from './components/carousel'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Login from './formValidation2/Login'
+import SignUp from './formValidation2/SignUp'
+import { Router } from './routing/Router'
+import './index.css'
+import Counter from './Redux/Slice'
+import Btn from './Redux/Btn'
+ 
+
+
+function App() {
+  return (
+    <>
+    {/* <Header/> */}
+    <Router/>
+    {/* <Footer/> */}
+   {/* <Btn /> */}
+ 
+      {/* <Router/> */}
+
+    </>
+  )
+}
+
+export default App
