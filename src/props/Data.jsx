bb33f8aeeba0d0a68a03453eee3dd59f322e2e05
Shaahid <mohammadshaahid05@gmail.com>
2026-05-10 10:35:29 +0530

    Move

---
+++ src/props/Data.jsx
@@ -0,0 +1,30 @@
+export const moviesData = [
+  {
+    actorName: "Shah Rukh Khan",
+    totalMovies: 80,
+    age: 58,
+    movieName: "Jawan",
+    boxOfficeCollection: "1148 Cr"
+  },
+  {
+    actorName: "Prabhas",
+    totalMovies: 25,
+    age: 44,
+    movieName: "Baahubali 2",
+    boxOfficeCollection: "1810 Cr"
+  },
+  {
+    actorName: "Allu Arjun",
+    totalMovies: 20,
+    age: 42,
+    movieName: "Pushpa",
+    boxOfficeCollection: "373 Cr"
+  },
+  {
+    actorName: "Salman Khan",
+    totalMovies: 75,
+    age: 58,
+    movieName: "Tiger Zinda Hai",
+    boxOfficeCollection: "565 Cr"
+  }
+];
