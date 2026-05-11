cef46a2cd76d36de3bccf06a29dbd9412be51cba
Shaahid <mohammadshaahid05@gmail.com>
2026-05-11 16:34:38 +0530

    Move

---
+++ src/Redux/Btn.jsx
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux"
+import { DEC, INC } from "./Slice"
+
+const Btn = () => {
+    let counter = useSelector(state => state.count)
+    let dispatch = useDispatch()
+    return(
+        <>
+            <center>
+                <h1>count:{counter}</h1>
+                <button className="btn btn-primary btn-outline-primary" onClick={()=>dispatch(INC())}>INC</button>
+                <button className="btn btn-danger btn-outline-danger" onClick={()=>dispatch(DEC())}>DEC</button>
+            </center>
+        </>
+    )
+}
+
+export default Btn
