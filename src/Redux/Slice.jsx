dc5187848cb1e2e17e96d064fecb6b26574771f4
Shaahid <mohammadshaahid05@gmail.com>
2026-05-11 16:35:13 +0530

    Move

---
+++ src/Redux/Slice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+    let counterSlice = createSlice({
+        name:'count',
+        initialState:0,
+        reducers:{
+            INC: (state,action) => (state+1),
+            DEC: (state,action) => (state-1)
+        }
+    })
+
+
+export const {INC,DEC} = counterSlice.actions
+export default counterSlice.reducer
+
