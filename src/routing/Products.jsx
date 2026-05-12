96df41825be6a1975e35eee7fcf2f5249015f988
Shaahid <mohammadshaahid05@gmail.com>
2026-05-12 15:04:29 +0530

    Move

---
+++ src/routing/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products_List from '../features/Products_List'
+
+
+function Products() {
+  return (
+    <div>
+      <h1 className='text-center my-3'>Products</h1>
+      <Products_List/>
+    </div>
+  )
+}
+
+export default Products
+  
