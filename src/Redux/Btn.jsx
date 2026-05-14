990f2e4a817271b6e342bdd6eb6e4b8c2992e20e
Shaahid <mohammadshaahid05@gmail.com>
2026-05-14 16:19:39 +0530

    Move

--- src/Redux/Btn.jsx
+++ src/Redux/Btn.jsx
@@ -4,12 +4,12 @@ import { DEC, INC } from "./Slice"
 const Btn = () => {
     let counter = useSelector(state => state.count)
     let dispatch = useDispatch()
-    return(
+    return (
         <>
             <center>
                 <h1>count:{counter}</h1>
-                <button className="btn btn-primary btn-outline-primary" onClick={()=>dispatch(INC())}>INC</button>
-                <button className="btn btn-danger btn-outline-danger" onClick={()=>dispatch(DEC())}>DEC</button>
+                <button className="btn btn-primary btn-outline-primary " onClick={() => dispatch(INC())}>INC</button>
+                <button className="btn btn-danger btn-outline-danger" onClick={() => dispatch(DEC())}>DEC</button>
             </center>
         </>
     )
