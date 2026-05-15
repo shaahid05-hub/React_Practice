88c9e69a6064336e3675c4fdb4e5874f1de72463
Shaahid <mohammadshaahid05@gmail.com>
2026-05-15 11:04:30 +0530

    Move

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="bg-dark text-white text-center p-4 mt-5">
-      <h5>Acheivers IT</h5>
+      <h5 >Acheivers IT</h5>
       <h5>Devwares</h5>
       <p>High quality resources for developers</p>
       <p>© 2020 All rights reserved</p>
